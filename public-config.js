61f07425d4f14f9ff2233e37f8db23a0c89b421c
Mustafa Pakdil <mpakdil0@gmail.com>
2026-07-31 00:14:54 +0300

    deploy public account deletion page

---
+++ public-config.js
@@ -0,0 +1,5 @@
+window.STUDYTRACK_PUBLIC_CONFIG = Object.freeze({
+  supabaseUrl: "https://jamengjvnvskaoqipbxg.supabase.co",
+  supabasePublishableKey: "sb_publishable_vb4Masr6Kz8J63B6GEnw9A_1_rkLyhm",
+  supportEmail: "mpakdil0@gmail.com",
+});
